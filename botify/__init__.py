7393fc3bea530905aaf446e68b41c6e9cc6819f5
Timoo4devv357 <pseowner@gmail.com>
2024-12-19 18:49:12 +0100

    change: init

--- botify/__init__.py
+++ botify/__init__.py
@@ -1,5 +1,7 @@
+__title__ = "botify-api"
+__license__ = "MIT"
 __version__ = "0.0.1"
 
-from .api import BotifyAPI
+from .api import AsyncBotifyAPI, BotifyAPI
 from .errors import *
 from .models import *
